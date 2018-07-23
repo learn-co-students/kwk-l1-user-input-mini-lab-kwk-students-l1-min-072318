e4606da476b1df1e8e03df062ca59ed9b923ea3b
ziesop20 <sincerelysophie@hotmail.com>
2018-07-23 20:44:31 +0000

    Done.

---
+++ invitation.rb
@@ -0,0 +1,14 @@
+puts "Who are you inviting?"
+name = gets.chomp 
+puts "Where are you having your party?"
+location = gets.chomp 
+puts "How many people are you inviting?"
+people = gets.chomp 
+puts "What should everyone wear?"
+clothing = gets.chomp 
+puts "What are you celebrating?"
+celebration = gets.chomp 
+puts "What time does the party start?"
+time = gets.chomp 
+
+puts "Dear, #{name} we are having a party at #{location}. There will be #{people} there, so dress your best in #{clothing}. We are getting together to celebrate #{celebration}, a big deal and a worthy cause for a gathering. The party starts promptly at #{time}, so be prompt. Love you dear ones, you will forever be my lovelies. Xoxo"
